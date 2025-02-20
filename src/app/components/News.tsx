f861dc55fd2cc67c4dd929e509b80a74e66dd135
Kame <121649992+Kamerito@users.noreply.github.com>
2025-02-20 14:08:11 +0900

    ニュース更新(部活委員会追加)

--- src/app/components/News.tsx
+++ src/app/components/News.tsx
@@ -5,16 +5,16 @@ const News = () => {
       <div className="space-y-4">
         <div className="border-b border-gray-300 pb-4">
           <div className="flex flex-col sm:flex-row justify-start">
-            <p className="sm:mr-[10%] sm:ml-[20%]">2024.2.14</p>
-            <p>中学生徒会選挙2024特設サイト開設！</p>
+            <p className="sm:mr-[10%] sm:ml-[20%]">2024.2.20</p>
+            <p>立候補者の部活と委員会の情報を追加しました。</p>
           </div>
         </div>
       </div>
       <div className="pb-4">
-        {/* <div className="flex justify-start mt-4">
+        <div className="flex justify-start mt-4">
           <p className="mr-[10%] ml-[20%]">2024.2.14</p>
           <p>中学生徒会選挙2024特設サイト開設！</p>
-        </div> */}
+        </div> 
         {/* 追加のニュース項目をここに追加できます */}
       </div>
     </div>
